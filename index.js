c82faef43cd9bee2f0f841b21d80fb176190b7ed
Frank Dillon <dillonf2@outlook.com>
2023-03-02 17:16:47 -0500

    array filter method lab completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution here
+const { objectContaining } = require("expect")
+
+function findMatching(array, string){
+      return((array.filter(function(element){
+      return (string.toLowerCase()==element.toLowerCase())
+     })))
+     
+    }
+
+function fuzzyMatch(array,string){
+    return(array.filter(function(element){
+        return(string.substring(0,2)==element.substring(0,2))
+        })
+    )
+    }
+
+    function matchName (array,string){
+        return(array.filter(function(key){
+            return (string===key.name)
+        }))
+    }
